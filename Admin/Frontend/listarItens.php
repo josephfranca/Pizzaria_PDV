6e5ddfe233efdd659e07dfdcefb6a3001f6c18a5
josephfranca <116111572+josephfranca@users.noreply.github.com>
2026-06-17 16:06:49 -0300

    telas de itens finalizadas e telas de categorias em progresso
    telas de itens finalizadas e telas de categorias em progresso, faltam apenas duas telas da categoria, recuperar categoria e listar categorias, além da tela do caixa, após isso será criado o banco de dados.

---
+++ Admin/Frontend/listarItens.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de ITens = Cardápio</title>
+    <link rel="stylesheet" href="../Frontend/Assets/CSS/listarItens.css">
+</head>
+<body>
+    <div class="tabela-painel">
+        <div class="tabela-header">
+            <h1>Itens Cadastrados (Cardápio)</h1>
+        </div>
+
+        <div class="tabela-conteudo">
+            <table class="tabela-dados">
+                <thead>
+                    <tr>
+                        <th class="col-id">ID do Item</th>
+                        <th class="col-nome">Nome do Item</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12</td>
+                        <td>Pizza Calabresa G</td>
+                    </tr>
+                    <tr>
+                        <td>15</td>
+                        <td>Pizza Frango com Catupiry</td>
+                    </tr>
+                    <tr>
+                        <td>22</td>
+                        <td>Coca-Cola 2L</td>
+                    </tr>
+                    <tr>
+                        <td>23</td>
+                        <td>Suco de Laranja 500ml</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="tabela-rodape">
+            <a href="telaitens.html" class="btn-voltar">Voltar</a>
+        </div>
+    </div>
+</body>
+</html>
