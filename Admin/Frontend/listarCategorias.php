804281c3c44ea3caa8e73014c1c7ce0f50d82b67
josephfranca <116111572+josephfranca@users.noreply.github.com>
2026-06-23 17:41:01 -0300

    tela categorias e listar categorias feitas
    tela categorias e listar categorias feitas, falta apenas a tela do caixa para acabar as telas

---
+++ Admin/Frontend/listarCategorias.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Categorias - ADM</title>
+    <link rel="stylesheet" href="../Frontend/Assets/CSS/listarCategorias.css">
+</head>
+<body>
+    <div class="tabela-painel">
+        <div class="tabela-header">
+            <h1>Categorias Registradas</h1>
+            <p class="subtitulo">Visão geral dos agrupamentos do cardápio</p>
+        </div>
+
+        <div class="tabela-conteudo">
+            <table class="tabela-dados">
+                <thead>
+                    <tr>
+                        <th>Nome Categoria</th>
+                        <th>Quantidade Produtos</th>
+                        <th>Status</th>
+                        <th>Data de criação</th>
+                        <th>ID Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Pizzas Tradicionais</td>
+                        <td>14 itens</td>
+                        <td><span class="badge status=ativo">Ativo</span></td>
+                        <td>23/06/2026</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>Pizzas Doces</td>
+                        <td>0 itens</td>
+                        <td><span class="badge status=ativo">Desativado</span></td>
+                        <td>23/06/2026</td>
+                        <td>2</td>
+                    </tr>
+                    <tr>
+                        <td>Bebidas</td>
+                        <td>8 itens</td>
+                        <td><span class="badge status=ativo">Ativo</span></td>
+                        <td>23/06/2026</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Combos & Promoções</td>
+                        <td>3 itens</td>
+                        <td><span class="badge status=ativo">Ativo</span></td>
+                        <td>23/06/2026</td>
+                        <td>4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="tabela-rodape">
+            <a href="telaCategorias.html" class="btn-voltar">Voltar</a>
+        </div>
+    </div>
+</body>
+</html>
